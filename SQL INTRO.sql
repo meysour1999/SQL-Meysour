1b942f1c3b716a2faebedb1cdf212f8f32a08dbc
Meysour Omezzine <meysour.o@gmail.com>
2023-02-28 17:05:23 +0100

    new file

---
+++ SQL INTRO.sql
@@ -0,0 +1,16 @@
+USE sakila;
+
+SELECT *  FROM actor; 
+SELECT *  FROM film;
+SELECT * FROM customer;
+
+SELECT title from film;
+
+SELECT name as language from language;
+SELECT count(store_id) from store;
+SELECT count(staff_id) from staff;
+SELECT first_name from staff; 
+
+
+
+
